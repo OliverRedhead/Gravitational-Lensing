d0bb65499ae8f16b817710845ef1a2151b6d6459
Oliver Redhead <ollieranga@gmail.com>
2026-01-22 10:36:42 +1100

    added get_potential() method for visualisation

--- wk1/PIEMD/piemd.py
+++ wk1/PIEMD/piemd.py
@@ -48,11 +48,8 @@ class PIEMD(Deflector):
         Rx = X - cx
         Ry = Y - cy
 
-        cp = np.cos(self.phi)
-        sp = np.sin(self.phi)
-
-        Rxp =  cp * Rx + sp * Ry
-        Ryp = -sp * Rx + cp * Ry
+        Rxp =  np.cos(self.phi) * Rx + np.sin(self.phi) * Ry
+        Ryp = -np.sin(self.phi) * Rx + np.cos(self.phi) * Ry
 
         Re = np.maximum(
             np.sqrt(self.e**2 * (self.s**2 + Rxp**2) + Ryp**2),
@@ -60,6 +57,59 @@ class PIEMD(Deflector):
         )
 
         return self.theta_E / (2 * Re)
+
+    def get_potential(self, source: Source):
+        src = source.array
+        nx, ny, *_ = src.shape
+
+        cx = (nx - 1) / 2
+        cy = (ny - 1) / 2
+
+        x = np.arange(0, nx)
+        y = np.arange(0, ny)
+        X, Y = np.meshgrid(x, y, indexing="xy")
+
+        Rx = X - cx
+        Ry = Y - cy
+
+        Rxp =  np.cos(self.phi) * Rx + np.sin(self.phi) * Ry
+        Ryp = -np.sin(self.phi) * Rx + np.cos(self.phi) * Ry
+
+        Re = np.maximum(
+            np.sqrt(self.e**2 * (self.s**2 + Rxp**2) + Ryp**2),
+            1e-12
+        )
+
+        Rf = np.sqrt((1-self.e**2) * Rxp**2 + (Re + self.s)**2) 
+
+        eps = 1e-12
+        if abs(1 - self.e) < 1e-6:
+            rc = np.sqrt(Rxp**2 + Ryp**2 + self.s**2)
+            alpha_x = self.theta_E * Rxp / (rc + self.s + eps)
+            alpha_y = self.theta_E * Ryp / (rc + self.s + eps)
+        
+        else:
+            a = np.sqrt(1 - self.e**2)
+
+            denom_x = Re + self.s
+            denom_y = Re + self.e**2 * self.s
+
+            denom_x = np.maximum(denom_x, eps)
+            denom_y = np.maximum(denom_y, eps)
+
+            u_x = a * Rxp / denom_x
+            u_y = a * Ryp / denom_y
+
+            # clip to valid domain for arctanh
+            u_y = np.clip(u_y, -1 + 1e-12, 1 - 1e-12)
+
+            alpha_x = self.theta_E * (1 / a) * np.arctan(u_x)
+            alpha_y = self.theta_E * (1 / a) * np.arctanh(u_y)
+
+        psi =  Rxp * alpha_x + Ryp * alpha_y - self.s * np.log(Rf)
+
+        return self.theta_E * psi
+
 
     def get_image(self, source: Source) -> np.ndarray:
         src = source.array
