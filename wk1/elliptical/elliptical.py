8c4f09a84f1f2ddd9b271aced72498eeff870b88
Oliver Redhead <ollieranga@gmail.com>
2026-01-21 11:50:42 +1100

    getter methods to visualise convergence and lensing potential

--- wk1/elliptical/elliptical.py
+++ wk1/elliptical/elliptical.py
@@ -4,11 +4,76 @@ from base_classes import Source, Deflector
 class EllipticalDeflector(Deflector):
 
     def __init__(self, theta_E : float, gamma_1: float, gamma_2: float) -> None:
+        """
+        Initialise an ellitpical deflector instance. 
+
+        Parameters
+        ---------
+        theta_E : float
+            A float representing the angular Einstein radius
+        gamma_1 : float
+            A float representing the xy component of shear
+        gamma_2 : float 
+            A float representing the diagonal component of shear
+        """
         super().__init__()
         self.theta_E = theta_E
         self.g1 = gamma_1
         self.g2 = gamma_2
 
+    def get_convergence(self, source: Source):
+        """
+        A getter method for visualising the surface density of the lens.
+
+        Parameters
+        ----------
+        source : Source
+            A source to set the dimensions of the output convergence field
+        """
+        
+        nx, ny, *_ = source.array.shape
+        cx = (nx-1)/2
+        cy = (nx-1)/2
+        
+        x = np.arange(0, nx, 1)
+        y = np.arange(0, ny, 1)
+        X, Y = np.meshgrid(x,y, indexing='xy')
+
+        Rx = X - cx
+        Ry = Y - cy
+        theta = np.sqrt(Rx**2 + Ry**2)
+        theta[theta == 0] = 1e-6
+
+        kappa = self.theta_E / (2 * theta)
+
+        return kappa
+    
+    def get_potential(self, source: Source):
+        """
+        A getter method for visualising the lensing potential of the lens.
+
+        Parameters
+        ----------
+        source : Source
+            A source to set the dimensions of the output potential field
+        """
+        
+        nx, ny, *_ = source.array.shape
+        cx = (nx-1)/2
+        cy = (nx-1)/2
+        
+        x = np.arange(0, nx, 1)
+        y = np.arange(0, ny, 1)
+        X, Y = np.meshgrid(x,y)
+
+        Rx = X - cx
+        Ry = Y - cy
+        theta = np.sqrt(Rx**2 + Ry**2)
+        theta[theta == 0] = 1e-6
+
+        phi = self.theta_E * theta + self.g1 * (Rx**2 - Ry**2) + 2 * self.g2 * Rx * Ry
+
+        return phi
 
     def get_image(self, source: Source) -> np.ndarray:
         src = source.array
